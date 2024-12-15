e3f2d610838d534a5626cb11a89fe0c0585e9b69
Nguyen Khanh Hoang with Koang <khanhhoang24052004@gmail.com>
2024-12-16 01:37:53 +0700

    video8.1

---
+++ video8.1/video8.1.js
@@ -0,0 +1,25 @@
+console.log('Hello world')
+// key: value
+let obj = {
+    name: 'Koang', 
+    address:'Sgon',
+    tuoi: 25,
+    email: '',
+    gender:'',
+    
+};
+
+
+
+let b = 'name';
+
+obj[b] = 'Eric';
+
+//obj.name = 'Eric';
+
+
+
+
+
+console.log('what is your name?',`My name is:`,obj.name);
+console.log('Where do you live?',`I'm live in:`,obj.address);
