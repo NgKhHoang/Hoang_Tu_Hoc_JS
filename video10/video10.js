64e01d3396675f5b25b20f3a7ce61e7594dd25fb
Nguyen Khanh Hoang with Koang <khanhhoang24052004@gmail.com>
2024-12-18 18:26:35 +0700

    video11

---
+++ video10/video10.js
@@ -0,0 +1,5 @@
+console.log("hello world from html");
+
+let a = 5;
+let b = '5';
+console.log(typeof a, typeof b, 'result:', a===b);
---
+++ video11/video11.js
@@ -0,0 +1,9 @@
+console.log("hi")
+
+var i = 0;
+let arr = ['MU', 'Man', 'Liver', 'Chelsea']
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+console.log(typeof i,i)
