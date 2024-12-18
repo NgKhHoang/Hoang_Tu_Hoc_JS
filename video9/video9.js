e8bdf563722fbff28f29b255761949d9d8ae3c2f
Nguyen Khanh Hoang with Koang <khanhhoang24052004@gmail.com>
2024-12-18 17:05:27 +0700

    knowledge_Array

---
+++ video9/video9.js
@@ -0,0 +1,18 @@
+console.log('Hello World with Array')
+
+
+let a = {
+    name: 'Koang',
+    age: 20,
+
+};
+//key:value
+console.log(typeof a);
+console.log(a.age);
+
+let b = ['Mu', 'chelsea', 'Liver'];
+console.log(typeof b, b);   
+console.log(typeof b[0]);   
+
+
+
